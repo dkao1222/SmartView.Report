4a6ac9b360a6e878075babc8adbe823506829982
dkao1222 <dkao1222@gmail.com>
2021-03-02 01:59:12 +0800

    Change sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -56,9 +56,9 @@ Future<String> signInWithGoogle() async {
     imageUrl = user.photoURL;
 
     // Only taking the first part of the name, i.e., First Name
-    if (name.contains(" ")) {
+    /*if (name.contains(" ")) {
       name = name.substring(0, name.indexOf(" "));
-    }
+    }*/
 
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);

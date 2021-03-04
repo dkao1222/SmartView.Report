5579c02b59229bd92efde948e05d5fb9de322b03
dkao1222 <dkao1222@gmail.com>
2021-03-04 15:55:45 +0800

    Update Title

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login',
+      title: 'SmartView Web Tools',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

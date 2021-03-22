1adac0964801046f7eda3b5138932136f284e87c
Dragon Kao <dkao1222@gmail.com>
2021-03-22 12:21:38 +0800

    Update _shp_table_detail.dart

--- lib/_shp_table_detail.dart
+++ lib/_shp_table_detail.dart
@@ -30,15 +30,15 @@ class ShppingDetail extends StatelessWidget {
                 */
             var shipmentDate = DateTime.tryParse(
                     snapshot.data[i].shipmentDateTime.toString() ?? '')
-                .add(Duration(hours: -8));
+                //.add(Duration(hours: -8));
             //snapshot.data[i].shipmentDateTime.toString().trim()
             var serviceStart = DateTime.tryParse(
                     snapshot.data[i].serviceStart.toString() ?? '')
-                .add(Duration(hours: -8));
+                //.add(Duration(hours: -8));
 
             var serviceEnd =
                 DateTime.tryParse(snapshot.data[i].serviceEnd.toString() ?? '')
-                    .add(Duration(hours: -8));
+                    //.add(Duration(hours: -8));
 
             var checkDateTime = serviceEnd.difference(DateTime.now());
 
